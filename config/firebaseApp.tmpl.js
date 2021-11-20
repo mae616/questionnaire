7ec16743f6f97b9172c0a7b788a4f933134c4efa
mae616 <oya2.mi@hotmail.co.jp>
2021-11-20 21:32:23 +0900

    firebaseApp.tmpl.js

---
+++ config/firebaseApp.tmpl.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "***********************",
+    authDomain: "***********************",
+    projectId: "***********************",
+    storageBucket: "***********************",
+    messagingSenderId: "***********************",
+    appId: "***********************"
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+
+const db = getFirestore();
+
+export { db, collection, addDoc }
